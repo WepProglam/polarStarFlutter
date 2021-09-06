150b12deed46e05c87e1d5ff1c794df136501542
ByungWoo <63662808+bw-99@users.noreply.github.com>
2021-09-07 01:51:35 +0900

    시간표 기본 레이아웃

--- lib/app/ui/android/timetable/timetable.dart
+++ lib/app/ui/android/timetable/timetable.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:polarstar_flutter/app/controller/timetable/timetable_controller.dart';
 import 'package:polarstar_flutter/app/ui/android/timetable/widgets/table_list.dart';
-import 'package:polarstar_flutter/app/ui/android/widgets/app_bar.dart';
 
 import 'package:polarstar_flutter/app/ui/android/widgets/bottom_navigation_bar.dart';
 
@@ -57,10 +56,12 @@ class Timetable extends StatelessWidget {
               ]),
               Container(
                 width: size.width,
-                color: Colors.black,
                 margin: const EdgeInsets.only(top: 15.5),
-                height: 60.0 * 12,
-                child: TimeTable(timeTableController: timeTableController),
+                height: 44 + 60.0 * 11,
+                child: TimeTable(
+                  timeTableController: timeTableController,
+                  width: size.width,
+                ),
               ),
               Container(
                 margin: const EdgeInsets.only(left: 15, top: 20.3),
@@ -93,26 +94,146 @@ class Timetable extends StatelessWidget {
 }
 
 class TimeTable extends StatelessWidget {
-  const TimeTable({
-    Key key,
-    @required this.timeTableController,
-  }) : super(key: key);
+  const TimeTable(
+      {Key key, @required this.timeTableController, @required this.width})
+      : super(key: key);
 
   final TimeTableController timeTableController;
+  final double width;
 
   @override
   Widget build(BuildContext context) {
+    final List<String> days = [
+      "MON.",
+      "Tues.",
+      "Wed.",
+      "Thur.",
+      "Fri.",
+      "Sat.",
+      "Sun."
+    ];
     return Column(
       children: [
         Container(
-          height: 60.0 * 12,
+          height: 44 + 60.0 * 11,
           child: ListView.builder(
               itemCount: 12,
+              physics: NeverScrollableScrollPhysics(),
               itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  height: 60,
-                  color: Colors.white10,
-                );
+                if (index == 0) {
+                  return Container(
+                    height: 44,
+                    // decoration: BoxDecoration(
+                    //     color: Colors.white,
+                    //     border: Border(
+                    //         bottom:
+                    //             BorderSide(color: Colors.black, width: 0.5))),
+                    child: TimeTableDays(width: width, days: days),
+                  );
+                } else {
+                  return Container(
+                    height: 60,
+                    child: Row(
+                      children: [
+                        Container(
+                          width: width,
+                          child: ListView.builder(
+                              itemCount: 8,
+                              scrollDirection: Axis.horizontal,
+                              itemBuilder: (BuildContext context, int i) {
+                                if (i == 0) {
+                                  return Container(
+                                    width: width / 12,
+                                    decoration: BoxDecoration(
+                                        color: Colors.grey,
+                                        border: Border(
+                                            right: BorderSide(
+                                                color: Colors.black,
+                                                width: 0.5),
+                                            bottom: BorderSide(
+                                                color: Colors.black,
+                                                width: 0.5))),
+                                    child: Center(
+                                      child: Text("${index + 8}",
+                                          style: const TextStyle(
+                                              color: const Color(0xff333333),
+                                              fontWeight: FontWeight.w700,
+                                              fontFamily: "PingFangSC",
+                                              fontStyle: FontStyle.normal,
+                                              fontSize: 14.0),
+                                          textAlign: TextAlign.left),
+                                    ),
+                                  );
+                                } else {
+                                  return Container(
+                                    width: (width * (11 / 12)) / 7,
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        border: Border(
+                                            right: BorderSide(
+                                                color: Colors.black,
+                                                width: 0.5),
+                                            bottom: BorderSide(
+                                                color: Colors.black,
+                                                width: 0.5))),
+                                  );
+                                }
+                              }),
+                        )
+                      ],
+                    ),
+                  );
+                }
+              }),
+        )
+      ],
+    );
+  }
+}
+
+class TimeTableDays extends StatelessWidget {
+  const TimeTableDays({
+    Key key,
+    @required this.width,
+    @required this.days,
+  }) : super(key: key);
+
+  final double width;
+  final List<String> days;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: width,
+          child: ListView.builder(
+              itemCount: 8,
+              scrollDirection: Axis.horizontal,
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (BuildContext context, int i) {
+                if (i == 0) {
+                  return Container(
+                    width: width / 12,
+                    color: Colors.grey,
+                  );
+                } else {
+                  return Container(
+                    width: (width * (11 / 12)) / 7,
+                    color: Colors.grey,
+                    child: Center(
+                      child: Text(
+                        "${days[i - 1]}",
+                        style: const TextStyle(
+                            color: const Color(0xff333333),
+                            fontWeight: FontWeight.w700,
+                            fontFamily: "PingFangSC",
+                            fontStyle: FontStyle.normal,
+                            fontSize: 14.0),
+                      ),
+                    ),
+                  );
+                }
               }),
         )
       ],
