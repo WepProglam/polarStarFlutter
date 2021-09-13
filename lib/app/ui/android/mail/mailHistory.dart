61f11971386201ae73aa5ec54df6d67abbd22794
rudxor02 <rudxor07@naver.com>
2021-09-13 22:51:32 +0900

    mailHistory

--- lib/app/ui/android/mail/mailHistory.dart
+++ lib/app/ui/android/mail/mailHistory.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:polarstar_flutter/app/controller/mail/mail_controller.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class MailHistory extends StatelessWidget {
   final MailController mailController = Get.find();
@@ -26,9 +27,14 @@ class MailHistory extends StatelessWidget {
             leadingWidth: 35,
             titleSpacing: 0,
             title: Text(
-              "${mailController.opponentProfile.value.PROFILE_NICKNAME}",
-              style: TextStyle(color: Colors.black),
-            ),
+                "${mailController.opponentProfile.value.PROFILE_NICKNAME}",
+                style: const TextStyle(
+                    color: const Color(0xff333333),
+                    fontWeight: FontWeight.w700,
+                    fontFamily: "PingFangSC",
+                    fontStyle: FontStyle.normal,
+                    fontSize: 21.0),
+                textAlign: TextAlign.left),
           ),
           body: RefreshIndicator(
             onRefresh: mailController.getMail,
@@ -44,7 +50,7 @@ class MailHistory extends StatelessWidget {
                         itemCount: mailController.mailHistory.length,
                         scrollDirection: Axis.vertical,
                         shrinkWrap: true,
-                        padding: EdgeInsets.only(top: 10, bottom: 10),
+                        padding: EdgeInsets.only(top: 10, bottom: 97),
                         itemBuilder: (context, index) {
                           if (index == mailController.mailHistory.length - 1) {
                             print((mailController.mailHistory[index].FROM_ME));
@@ -52,21 +58,82 @@ class MailHistory extends StatelessWidget {
                           return Container(
                               padding: (mailController.mailHistory[index].FROM_ME == 0
                                   ? EdgeInsets.only(left: 15, bottom: 33.5)
-                                  : EdgeInsets.only(right: 15, top: 26.5)),
+                                  : EdgeInsets.only(right: 15, bottom: 26.5)),
                               child: Align(
                                   alignment:
                                       (mailController.mailHistory[index].FROM_ME == 0
                                           ? Alignment.topLeft
                                           : Alignment.topRight),
-                                  child: (mailController.mailHistory[index].FROM_ME == 0
-                                      ? Container(
-                                          width: 41.5,
-                                          height: 41.5,
-                                          decoration: BoxDecoration(
-                                              border: Border.all(
+                                  child: (mailController
+                                              .mailHistory[index].FROM_ME ==
+                                          0
+                                      ? Row(children: [
+                                          Container(
+                                              width: 41.5,
+                                              height: 41.5,
+                                              margin:
+                                                  EdgeInsets.only(right: 14),
+                                              child: CachedNetworkImage(
+                                                  imageUrl:
+                                                      'http://ec2-3-37-156-121.ap-northeast-2.compute.amazonaws.com:3000/uploads/${mailController.opponentProfile.value.PROFILE_PHOTO}',
+                                                  imageBuilder: (context,
+                                                          imageProvider) =>
+                                                      Container(
+                                                          decoration: BoxDecoration(
+                                                              shape: BoxShape
+                                                                  .circle,
+                                                              image: DecorationImage(
+                                                                  image:
+                                                                      imageProvider,
+                                                                  fit: BoxFit
+                                                                      .fill))),
+                                                  fadeInDuration:
+                                                      Duration(milliseconds: 0),
+                                                  progressIndicatorBuilder: (context,
+                                                          url,
+                                                          downloadProgress) =>
+                                                      Image(
+                                                          image: AssetImage(
+                                                              'assets/images/spinner.gif')),
+                                                  errorWidget:
+                                                      (context, url, error) {
+                                                    print(error);
+                                                    return Icon(Icons.error);
+                                                  })),
+                                          Container(
+                                              constraints:
+                                                  BoxConstraints(maxWidth: 260),
+                                              decoration: BoxDecoration(
+                                                  borderRadius: BorderRadius.only(
+                                                      topLeft:
+                                                          Radius.circular(36),
+                                                      topRight:
+                                                          Radius.circular(36),
+                                                      bottomRight:
+                                                          Radius.circular(36)),
+                                                  border: Border.all(
+                                                      color: const Color(
+                                                          0xffdcdcdc),
+                                                      width: 1),
                                                   color:
-                                                      const Color(0x59ffffff),
-                                                  width: 3)))
+                                                      const Color(0xfff2f2f2)),
+                                              child: Container(
+                                                  margin: EdgeInsets.only(
+                                                      left: 11.5,
+                                                      top: 13,
+                                                      right: 13,
+                                                      bottom: 16),
+                                                  child: Text(
+                                                      "${mailController.mailHistory[index].CONTENT}",
+                                                      style: const TextStyle(
+                                                          color:
+                                                              const Color(0xff333333),
+                                                          fontWeight: FontWeight.w700,
+                                                          fontFamily: "PingFangSC",
+                                                          fontStyle: FontStyle.normal,
+                                                          fontSize: 16.0),
+                                                      textAlign: TextAlign.left)))
+                                        ])
                                       : Container(
                                           constraints:
                                               BoxConstraints(maxWidth: 260),
@@ -74,10 +141,16 @@ class MailHistory extends StatelessWidget {
                                               borderRadius: BorderRadius.only(
                                                   topLeft: Radius.circular(36),
                                                   topRight: Radius.circular(36),
-                                                  bottomLeft: Radius.circular(36)),
-                                              border: Border.all(color: const Color(0xffdcdcdc), width: 1),
+                                                  bottomLeft:
+                                                      Radius.circular(36)),
+                                              border: Border.all(
+                                                  color:
+                                                      const Color(0xffdcdcdc),
+                                                  width: 1),
                                               color: const Color(0xff1a4678)),
-                                          child: Container(margin: EdgeInsets.only(left: 11.5, top: 13, right: 13, bottom: 16), child: Text("${mailController.mailHistory[index].CONTENT}", style: const TextStyle(color: const Color(0xffffffff), fontWeight: FontWeight.w700, fontFamily: "PingFangSC", fontStyle: FontStyle.normal, fontSize: 16.0), textAlign: TextAlign.left))))));
+                                          child: Container(
+                                              margin: EdgeInsets.only(left: 11.5, top: 13, right: 13, bottom: 16),
+                                              child: Text("${mailController.mailHistory[index].CONTENT}", style: const TextStyle(color: const Color(0xffffffff), fontWeight: FontWeight.w700, fontFamily: "PingFangSC", fontStyle: FontStyle.normal, fontSize: 16.0), textAlign: TextAlign.left))))));
                         },
                       ),
                     );
@@ -90,39 +163,48 @@ class MailHistory extends StatelessWidget {
           ),
           //입력창
           bottomSheet: Container(
-            height: 60,
-            child: Stack(children: [
-              //키보드
-              Container(
-                child: TextFormField(
-                  controller: commentWriteController,
-                  onFieldSubmitted: (value) {
-                    mailController.sendMailIn(
-                        commentWriteController.text, controller);
-                  },
-                  textInputAction: TextInputAction.done,
-                  decoration: InputDecoration(
-                      hintText: "쪽지 보내기", border: OutlineInputBorder()),
-                ),
-              ),
-              Positioned(
-                top: 15,
-                right: 20,
-                child: InkWell(
-                  onTap: () {
-                    mailController.sendMailIn(
-                        commentWriteController.text, controller);
-
-                    commentWriteController.clear();
-                  },
-                  child: Icon(
-                    Icons.send,
-                    size: 30,
-                  ),
-                ),
-              ),
-            ]),
-          )),
+              height: 107,
+              decoration: BoxDecoration(color: const Color(0xffffffff)),
+              child:
+                  //키보드
+                  Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                          width: MediaQuery.of(context).size.width - 30,
+                          height: 52,
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(52)),
+                              border: Border.all(
+                                  color: const Color(0xffd4d4d4), width: 1),
+                              color: const Color(0x05333333)),
+                          child: Stack(children: [
+                            Positioned(
+                                top: 14,
+                                left: 18.5,
+                                child: Container(
+                                  height: 21.5,
+                                  width: 144,
+                                  child: TextFormField(
+                                      controller: commentWriteController,
+                                      style: const TextStyle(
+                                          color: const Color(0xff333333),
+                                          fontWeight: FontWeight.w500,
+                                          fontFamily: "PingFangSC",
+                                          fontStyle: FontStyle.normal,
+                                          fontSize: 16.0),
+                                      onFieldSubmitted: (value) {
+                                        mailController.sendMailIn(
+                                            commentWriteController.text,
+                                            controller);
+                                      },
+                                      textInputAction: TextInputAction.done,
+                                      decoration: InputDecoration(
+                                        hintText: "Please enter content",
+                                        border: InputBorder.none,
+                                      )),
+                                ))
+                          ]))))),
     );
   }
 }
