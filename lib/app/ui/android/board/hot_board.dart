f6408659cab0f6eb705dc9cfd9c76375f1660e7e
ByungWoo <63662808+bw-99@users.noreply.github.com>
2021-09-24 19:49:14 +0900

    알림 목록 저장

--- lib/app/ui/android/board/hot_board.dart
+++ lib/app/ui/android/board/hot_board.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:polarstar_flutter/app/controller/board/board_controller.dart';
 import 'package:polarstar_flutter/app/controller/search/search_controller.dart';
+import 'package:polarstar_flutter/app/ui/android/board/board.dart';
 import 'package:polarstar_flutter/app/ui/android/board/widgets/board_layout.dart';
 import 'package:polarstar_flutter/app/ui/android/search/widgets/search_bar.dart';
 import 'package:polarstar_flutter/app/ui/android/widgets/app_bar.dart';
@@ -14,39 +15,71 @@ class HotBoard extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-            appBar: CustomAppBar(
-              pageName: "HOT",
-            ),
-            body: RefreshIndicator(
-              onRefresh: controller.refreshPage,
-              child: Stack(
-                children: [
-                  Column(
-                    children: [
-                      // 게시글 프리뷰 리스트
-                      Expanded(
-                        child: Obx(() {
-                          print(controller.httpStatus);
-                          if (controller.dataAvailablePostPreview.value) {
-                            return ListView.builder(
-                                controller: controller.scrollController.value,
-                                itemCount: controller.postBody.length,
-                                itemBuilder: (BuildContext context, int index) {
-                                  return PostPreview(
-                                    item: controller.postBody[index],
-                                  );
-                                });
-                          } else if (controller.httpStatus == 404) {
-                            return Text("아직 게시글이 없습니다.");
-                          } else {
-                            return Text("아직 게시글이 없습니다.");
-                          }
-                        }),
-                      ),
-                    ],
+            appBar: AppBar(
+              backgroundColor: Color(0xffffffff),
+              foregroundColor: Color(0xff333333),
+              elevation: 0,
+              automaticallyImplyLeading: false,
+              leadingWidth: 15 + 13.1 + 9.4,
+              leading: InkWell(
+                onTap: () {
+                  Get.back();
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 15, right: 13.1),
+                  child: Ink(
+                    child: Image.asset(
+                      'assets/images/848.png',
+                      fit: BoxFit.fitWidth,
+                    ),
                   ),
-                ],
+                ),
               ),
-            )));
+              titleSpacing: 0,
+              title: Container(
+                height: 40,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("핫보드",
+                        style: const TextStyle(
+                            color: const Color(0xff333333),
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "PingFangSC",
+                            fontStyle: FontStyle.normal,
+                            fontSize: 16.0),
+                        textAlign: TextAlign.left),
+                    Text("Sungkyunkwan University",
+                        style: const TextStyle(
+                            color: const Color(0xff333333),
+                            fontWeight: FontWeight.normal,
+                            fontFamily: "PingFangSC",
+                            fontStyle: FontStyle.normal,
+                            fontSize: 11.0),
+                        textAlign: TextAlign.left),
+                  ],
+                ),
+              ),
+            ),
+            body: Obx(() {
+              if (controller.dataAvailablePostPreview.value) {
+                return RefreshIndicator(
+                  onRefresh: () async {},
+                  child: ListView.builder(
+                      controller: controller.scrollController.value,
+                      itemCount: controller.postBody.length,
+                      physics: AlwaysScrollableScrollPhysics(),
+                      itemBuilder: (BuildContext context, int index) {
+                        return PostPreview(
+                          item: controller.postBody[index],
+                        );
+                      }),
+                );
+              } else if (controller.httpStatus == 404) {
+                return Text("아직 게시글이 없습니다.");
+              } else {
+                return Text("아직 게시글이 없습니다.");
+              }
+            })));
   }
 }
