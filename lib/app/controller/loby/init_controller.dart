b35e6244624e9c981c503883a64c5d30af027019
To-ToRo <ghkdcks1223@gmail.com>
2021-09-26 11:33:29 +0900

    로그인 버그 fix

--- lib/app/controller/loby/init_controller.dart
+++ lib/app/controller/loby/init_controller.dart
@@ -26,17 +26,30 @@ class InitController extends GetxController {
     switch (response["statusCode"]) {
       case 200:
         Get.snackbar("로그인 성공", "로그인 성공");
+
         break;
       default:
         Get.snackbar("로그인 실패", "로그인 실패");
     }
+    return response;
   }
 
   Future<bool> checkLogin() async {
-    if (box.hasData('isAutoLogin')) {
+    if (box.hasData('isAutoLogin') &&
+        box.hasData('id') &&
+        box.hasData('pw') &&
+        box.hasData('token')) {
       if (box.read('isAutoLogin')) {
-        await autoLogin(box.read('id'), box.read('pw'), box.read('token'));
-        return true;
+        await autoLogin(box.read('id'), box.read('pw'), box.read('token'))
+            .then((response) {
+          switch (response["statusCode"]) {
+            case 200:
+              return true;
+              break;
+            default:
+              return false;
+          }
+        });
       }
     }
     return false;
