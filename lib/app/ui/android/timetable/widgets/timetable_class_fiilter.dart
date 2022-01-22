961a359df09358b28847851c4d04e40ebdcc6dd1
ByungWoo <63662808+bw-99@users.noreply.github.com>
2022-01-22 15:40:00 +0900

    전공, 단과대 퍼블리싱

--- lib/app/ui/android/timetable/widgets/timetable_class_fiilter.dart
+++ lib/app/ui/android/timetable/widgets/timetable_class_fiilter.dart
@@ -25,33 +25,94 @@ class TimetableClassFilter extends StatelessWidget {
     final Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            toolbarHeight: 56,
+            automaticallyImplyLeading: false,
+            titleSpacing: 0,
+            title: Container(
+              width: size.width,
+              child: Container(
+                child: Stack(
+                  children: [
+                    Ink(
+                      child: InkWell(
+                        onTap: () {
+                          Get.back();
+                        },
+                        child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Image.asset(
+                              "assets/images/back_icon.png",
+                              width: 24,
+                              height: 24,
+                            )),
+                      ),
+                    ),
+                    Center(
+                      child: // 专业 / 领域
+                          Text("专业 / 领域",
+                              style: const TextStyle(
+                                  color: const Color(0xffffffff),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: "NotoSansSC",
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 16.0),
+                              textAlign: TextAlign.center),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
           // bottomNavigationBar:
           //     CustomBottomNavigationBar(mainController: mainController),
           body: Obx(() {
-        return ListView.builder(
-            itemCount: controller.college_name_list.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Ink(
-                height: 50,
-                child: InkWell(
-                  onTap: () async {
-                    int INDEX_COLLEGE_NAME =
-                        controller.college_name_list[index].INDEX_COLLEGE_NAME;
+            return ListView.builder(
+                itemCount: controller.college_name_list.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Ink(
+                    child: InkWell(
+                      onTap: () async {
+                        int INDEX_COLLEGE_NAME = controller
+                            .college_name_list[index].INDEX_COLLEGE_NAME;
 
-                    controller.INDEX_COLLEGE_NAME.value = INDEX_COLLEGE_NAME;
+                        controller.INDEX_COLLEGE_NAME.value =
+                            INDEX_COLLEGE_NAME;
 
-                    await controller.getMajorInfo();
+                        await controller.getMajorInfo();
 
-                    Get.toNamed(Routes.TIMETABLE_ADDCLASS_FILTER_MAJOR);
-                  },
-                  child: Center(
-                    child: Text(
-                        "${controller.college_name_list[index].COLLEGE_NAME}"),
-                  ),
-                ),
-              );
-            });
-      })),
+                        Get.toNamed(Routes.TIMETABLE_ADDCLASS_FILTER_MAJOR);
+                      },
+                      child: Column(children: [
+                        index == 0
+                            ? Container(
+                                height: 24,
+                              )
+                            : Container(),
+                        Container(
+                          padding: const EdgeInsets.symmetric(vertical: 11.5),
+                          child: Text(
+                            "${controller.college_name_list[index].COLLEGE_NAME}",
+                            style: const TextStyle(
+                                color: const Color(0xff6f6e6e),
+                                fontWeight: FontWeight.w500,
+                                fontFamily: "NotoSansKR",
+                                fontStyle: FontStyle.normal,
+                                fontSize: 14.0),
+                          ),
+                        ),
+                        // 선 21
+                        Container(
+                            height: 1,
+                            margin: const EdgeInsets.symmetric(horizontal: 20),
+                            decoration:
+                                BoxDecoration(color: const Color(0xffeaeaea)))
+                      ]),
+                    ),
+                  );
+                });
+          })),
     );
   }
 }
@@ -66,34 +127,98 @@ class TimetableClassMajor extends StatelessWidget {
     final Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            toolbarHeight: 56,
+            automaticallyImplyLeading: false,
+            titleSpacing: 0,
+            title: Container(
+              width: size.width,
+              child: Container(
+                child: Stack(
+                  children: [
+                    Ink(
+                      child: InkWell(
+                        onTap: () {
+                          Get.back();
+                        },
+                        child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Image.asset(
+                              "assets/images/back_icon.png",
+                              width: 24,
+                              height: 24,
+                            )),
+                      ),
+                    ),
+                    Center(
+                      child: // 专业 / 领域
+                          Text("专业 / 领域",
+                              style: const TextStyle(
+                                  color: const Color(0xffffffff),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: "NotoSansSC",
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 16.0),
+                              textAlign: TextAlign.center),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
           // bottomNavigationBar:
           //     CustomBottomNavigationBar(mainController: mainController),
           body: Obx(() {
-        return ListView.builder(
-            itemCount: controller.college_major_list.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Ink(
-                height: 50,
-                child: InkWell(
-                  onTap: () async {
-                    controller.INDEX_COLLEGE_MAJOR.value = controller
-                        .college_major_list[index].INDEX_COLLEGE_MAJOR;
-                    controller.college_major.value =
-                        controller.college_major_list[index].NAME;
+            return ListView.builder(
+                itemCount: controller.college_major_list.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Ink(
+                    child: InkWell(
+                      onTap: () async {
+                        controller.INDEX_COLLEGE_MAJOR.value = controller
+                            .college_major_list[index].INDEX_COLLEGE_MAJOR;
+                        controller.college_major.value =
+                            controller.college_major_list[index].NAME;
 
-                    String text = controller.search_name.value.trim();
-                    await controller.getClass(0);
-                    FocusScope.of(context).unfocus();
-                    Get.back();
-                    Get.back();
-                  },
-                  child: Center(
-                      child:
-                          Text("${controller.college_major_list[index].NAME}")),
-                ),
-              );
-            });
-      })),
+                        String text = controller.search_name.value.trim();
+                        await controller.getClass(0);
+                        FocusScope.of(context).unfocus();
+                        Get.back();
+                        Get.back();
+                      },
+                      child: Column(
+                        children: [
+                          index == 0
+                              ? Container(
+                                  height: 24,
+                                )
+                              : Container(),
+                          Container(
+                            padding: const EdgeInsets.symmetric(vertical: 11.5),
+                            child: Text(
+                              "${controller.college_major_list[index].NAME}",
+                              style: const TextStyle(
+                                  color: const Color(0xff6f6e6e),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: "NotoSansKR",
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 14.0),
+                            ),
+                          ),
+                          // 선 21
+                          Container(
+                              height: 1,
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 20),
+                              decoration:
+                                  BoxDecoration(color: const Color(0xffeaeaea)))
+                        ],
+                      ),
+                    ),
+                  );
+                });
+          })),
     );
   }
 }
